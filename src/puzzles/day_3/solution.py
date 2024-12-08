7ab808065a18644aff984321da1910b629918488
Konrad (PC) <konrad@guzek.uk>
2024-12-08 20:55:09 +0100

    complete day 3 part 2

--- src/puzzles/day_3/solution.py
+++ src/puzzles/day_3/solution.py
@@ -7,7 +7,6 @@ from ...puzzle_input import get_puzzle_input
 SOLUTION_DAY = 3
 
 MULTIPLICATION_PATTERN = re.compile(r"mul\((\d+),(\d+)\)")
-MULTIPLICATION_PATTERN_2 = re.compile(r"(?<=do\(\)).+?mul\((\d+),(\d+)\)")
 
 
 def solve_part_one(input_text: str):
@@ -18,9 +17,26 @@ def solve_part_one(input_text: str):
 
 def solve_part_two(input_text: str):
     """Calculates the solution for day 3, part two."""
-    # start_idx = 0
-    # # for i, c in enumerate(input_text):
-    # #     if  == "don't()"[start_idx]:
+    start_idx = None
+    enabled = True
+    result = 0
+    for i, c in enumerate(input_text):
+        if c == "d" or c == "m":
+            start_idx = i
+            continue
+        if start_idx is None or i - start_idx < 3:
+            continue
+        substring = input_text[start_idx : i + 1]
+        match substring:
+            case "do()":
+                enabled = True
+            case "don't()":
+                enabled = False
+            case _ if enabled and (
+                match := MULTIPLICATION_PATTERN.fullmatch(substring)
+            ):
+                result += int(match.group(1)) * int(match.group(2))
+    return result
 
 
 def main():
