27d8b525ea4e6e9d5e0f566f2e3edb14d7d99139
Konrad Guzek <konrad@guzek.uk>
2024-12-11 14:42:42 +0100

    implement functools cache

--- src/puzzles/day_11/solution.py
+++ src/puzzles/day_11/solution.py
@@ -1,37 +1,50 @@
 """Solution for puzzle 11, 2024-12-11"""
 
+from functools import cache
+
 from ...puzzle_input import get_puzzle_input
 
 SOLUTION_DAY = 11
 
 
+@cache
+def change_stone(stone: int) -> tuple[int, int | None]:
+    """Determines the next values of a stone after a blink."""
+    if stone == 0:
+        return 1, None
+    stone_str = str(stone)
+    num_digits = len(stone_str)
+    if num_digits % 2 == 0:
+        halfway_idx = num_digits // 2
+        return int(stone_str[:halfway_idx]), int(stone_str[halfway_idx:])
+    return stone * 2024, None
+
+
 def get_stones_after_blinks(
-    stones: list[str], times_blinked: int, blink_offset: int = 0
+    stones: list[int], times_blinked: int, blink_offset: int = 0
 ):
     """Computes the list of stones after the given number of blinks."""
     for blink in range(times_blinked):
-        print("Blink", blink + blink_offset)
-        new_stones: list[str] = []
-        for stone in stones:
-            stone_int = int(stone)
-            # To eliminate unnecessary leading zeros
-            stone = str(stone_int)
-            if stone_int == 0:
-                new_stones.append("1")
-            elif len(stone) % 2 == 0:
-                half_stone_length = len(stone) // 2
-                new_stones.append(stone[:half_stone_length])
-                new_stones.append(stone[half_stone_length:])
-            else:
-                new_stones.append(str(stone_int * 2024))
-        stones = new_stones
+        print(
+            f"Blink {blink + blink_offset + 1:>2}, number of stones: ",
+            end="",
+            flush=True,
+        )
+        to_append = []
+        for i, stone in enumerate(stones):
+            stone, extra_stone = change_stone(stone)
+            stones[i] = stone
+            if extra_stone is not None:
+                to_append.append(extra_stone)
+        stones += to_append
+        print(len(stones))
     return stones
 
 
 def main():
     """Entry point for the solution."""
-    input_text = get_puzzle_input(11).split()
-    stones_after_25_blinks = get_stones_after_blinks(input_text, 25)
+    stones = [int(stone) for stone in get_puzzle_input(11).split()]
+    stones_after_25_blinks = get_stones_after_blinks(stones, 25)
     print("Solution for day 11, part one:", len(stones_after_25_blinks))
     stones_after_75_blinks = get_stones_after_blinks(stones_after_25_blinks, 50, 25)
     print("Solution for day 11, part two:", len(stones_after_75_blinks))
