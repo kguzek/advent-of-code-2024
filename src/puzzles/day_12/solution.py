d0a57c4fde616dc3729c6851e5c25b6989bc5be7
Konrad Guzek <konrad@guzek.uk>
2024-12-13 14:44:14 +0100

    refactor to facilitate part 2 solution

--- src/puzzles/day_12/solution.py
+++ src/puzzles/day_12/solution.py
@@ -1,7 +1,5 @@
 """Solution for puzzle 12, 2024-12-12"""
 
-from itertools import product
-
 from ...puzzle_input import get_puzzle_input
 
 SOLUTION_DAY = 12
@@ -14,8 +12,10 @@ class Region:
         self.type = region_map[y][x]
         self.plots: set[tuple[int, int]] = set()
         self.perimeter = 0
+        self.number_of_sides = 0
         self._scan_plots(region_map, x, y)
-        self.fence_cost = len(self.plots) * self.perimeter
+        self.simple_fence_cost = len(self.plots) * self.perimeter
+        self.bulk_fence_cost = len(self.plots) * self.number_of_sides
 
     def contains_plot(self, x: int, y: int) -> bool:
         """Checks if the region contains a plot at the given coordinates."""
@@ -24,44 +24,51 @@ class Region:
     def _scan_plots(self, region_map: list[str], x: int, y: int) -> bool:
         """Scans the plots around the base plot for plots belonging to the region.
 
-        Returns a boolean indicating if the plot is on the edge of the region."""
+        Returns a tuple containing booleans indicating respectively if the plot is on the edge
+        of the region and if it is part of a new side."""
         if not 0 <= x < len(region_map[0]) or not 0 <= y < len(region_map):
-            return True
+            return True, False
         if not region_map[y][x] == self.type:
-            return True
+            return True, False
         self.plots.add((x, y))
         for delta_x, delta_y in ((0, -1), (1, 0), (0, 1), (-1, 0)):
             # Needed to avoid infinite loops
             if self.contains_plot(x + delta_x, y + delta_y):
                 continue
-            if self._scan_plots(region_map, x + delta_x, y + delta_y):
+            is_at_edge, is_new_side = self._scan_plots(
+                region_map, x + delta_x, y + delta_y
+            )
+            if is_at_edge:
                 self.perimeter += 1
-        return False
+            if is_new_side:
+                self.number_of_sides += 1
+        return False, False
 
 
-def solve_part_one(input_text: list[str]):
-    """Calculates the solution for day 12, part one."""
+def get_region_data(region_map: list[str]):
+    """Returns the regions in the region map."""
     regions: set[Region] = set()
-    total_cost = 0
-    for y, row in enumerate(input_text):
-        for x, _ in enumerate(row):
+    total_simple_cost = 0
+    total_bulk_cost = 0
+    height = len(region_map)
+    width = len(region_map[0])
+    for y in range(height):
+        for x in range(width):
             if any(region.contains_plot(x, y) for region in regions):
                 continue
-            region = Region(input_text, x, y)
+            region = Region(region_map, x, y)
             regions.add(region)
-            total_cost += region.fence_cost
-    return total_cost
-
-
-def solve_part_two(input_text: list[str]):
-    """Calculates the solution for day 12, part two."""
+            total_simple_cost += region.simple_fence_cost
+            total_bulk_cost += region.bulk_fence_cost
+    return total_simple_cost, total_bulk_cost
 
 
 def main():
     """Entry point for the solution."""
     input_text = get_puzzle_input(12).splitlines()
-    print("Solution for day 12, part one:", solve_part_one(input_text))
-    print("Solution for day 12, part two:", solve_part_two(input_text))
+    total_simple_cost, total_bulk_cost = get_region_data(input_text)
+    print("Solution for day 12, part one:", total_simple_cost)
+    print("Solution for day 12, part two:", total_bulk_cost)
 
 
 if __name__ == "__main__":
